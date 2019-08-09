a88aaffa6f5ef117ef9fd7be8f111ffc24a60206
Alex Debrecht <adebrech@ur.rochester.edu>
2019-08-09 12:01:53 -0400

    Add comments to Link class

--- XorDoublyLinkedList/Link.h
+++ XorDoublyLinkedList/Link.h
@@ -22,17 +22,21 @@ private:
 	E element;
 
 public:
+	// Shared null link pointer for easy null checking
 	static Link<E>* nullLink;
 
+	// Constructor with parameters
 	Link(const E& elem, Link<E>* prev, Link<E>* next) {
 		element = elem;
 		nextXORprev = XOR(next,prev);
 	}
 
+	// Default constructor
 	Link(Link<E>* prev = nullLink, Link<E>* next = nullLink) {
 		nextXORprev = XOR(next,prev);
 	}
 
+	// Overload new to use a freelist
 	void* operator new(size_t) {
 		if (freelist == nullLink) return ::new Link<E>;
 		Link<E>* temp = freelist;
@@ -40,6 +44,7 @@ public:
 		return temp;
 	}
 
+	// Overload delete to use a freelist
 	// Freelist is only singly-linked
 	void operator delete(void* ptr) {
 		((Link<E>*) ptr)->nextXORprev = freelist;
@@ -56,16 +61,21 @@ public:
 		return XOR(nextXORprev, next);
 	}
 
+	// Set nextXORprev with new next link
+	// First have to retrieve previous to preserve it
 	Link<E>* setNext(Link<E>* oldNext, Link<E>* newNext) {
 		nextXORprev = XOR(getPrev(oldNext), newNext); // retrieve pointer to prev link, then XOR with new next link
 		return nextXORprev;
 	}
 
+	// Set nextXORprev with new prev link
+	// First have to retrieve next to preserve it
 	Link<E>* setPrev(Link<E>* oldPrev, Link<E>* newPrev) {
 		nextXORprev = XOR(getNext(oldPrev), newPrev); // retrieve pointer to next link, then XOR with new previous link
 		return nextXORprev;
 	}
 
+	// Return element from link
 	const E& getVal() {
 		return element;
 	}
@@ -77,6 +87,7 @@ Link<E>* XOR(Link<E>* prev, Link<E>* next) {
 	return (Link<E>*) ((uintptr_t) prev ^ (uintptr_t) next);
 }
 
+// Initialize static member variables
 template <typename E>
 Link<E>* Link<E>::freelist = NULL;
 
